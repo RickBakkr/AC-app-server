291104551483f87f27f8033d0bb46573bd3a79e6
RickB2000 <rick.b@hotmail.com>
2014-01-26 02:22:56 +0100

    Added "getTimetable" function

--- lesrooster.class.php
+++ lesrooster.class.php
@@ -4,7 +4,14 @@
   *
   * @author Rick Bakker
   */
-Class Lesrooster extends Rooster {
+Class Lesrooster {
+ public function getTimetable($q, $week) {
+  if(empty($week) || !is_numeric($week) || !isset($week)) {
+   $week = date("W");
+  }
+  $curl = cURL::get("http://roosters.csg.nl/?q=" . $q . "&week=" . $week);
+  return $curl;
+ }
 
 }
 ?>
