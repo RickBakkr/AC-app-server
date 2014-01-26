7194c68189ff7202535c9a9d2a276d3371c728aa
RickB2000 <rick.b@hotmail.com>
2014-01-26 01:51:09 +0100

    Added "post" function

--- curl.class.php
+++ curl.class.php
@@ -23,4 +23,24 @@ Class cURL {
     }
     return $result;
   }
+  /**
+  * Function to do a post request
+  *
+  * @params string, string
+  * @return bool or string
+  */
+  public function get($url, $fields) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_POST, 1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if($httpCode != 200) {
+      return 'ERROR!';
+    }
+    return $result;
+  }
 }
