f8f0bc280727c46799ee49d72dfb63a4dd27d66d
RickB2000 <rick.b@hotmail.com>
2014-01-26 01:49:31 +0100

    Adding "get" function

---
+++ curl.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+  * cURL Class
+  *
+  * @author Rick Bakker
+  */
+Class cURL {
+  /**
+  * Function to do a get request
+  *
+  * @params string
+  * @return bool or string
+  */
+  public function get($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if($httpCode != 200) {
+      return 'ERROR!';
+    }
+    return $result;
+  }
+}
